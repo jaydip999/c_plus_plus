267c8e5d21085cf08cfafdd334534605656b3c85
Dhanji Kidecha <147084804+dhanjiRajput@users.noreply.github.com>
2024-02-02 15:29:15 +0530

    C++

---
+++ Arithmetic/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=10, b=5;
+
+    cout << "Enter A :" << a << endl;
+    cout << "Enter B :" << b;
+
+    return 0;
+}
---
+++ Arithmetic/2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter A : " ;
+    cin >> a;
+
+    cout << "Enter B : ";
+    cin >> b;
+    
+    cout << "sum : " << a+b;
+
+    return 0;
+}
